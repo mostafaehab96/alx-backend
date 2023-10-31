205c23d79c3f5962e72429583e5a0a54f51dfbd7
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-31 22:11:24 +0200

    Add task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Simple flask app."""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config(object):
+    """Configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Determines best local match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def hello():
+    """Return hello world"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
