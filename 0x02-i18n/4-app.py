75a78e61e21cf3068e596a51571e1c703024dfab
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-01 00:00:07 +0200

    Add task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Simple flask app."""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+class Config(object):
+    """Configuration class"""
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determines best local match"""
+    locale = request.args.get('locale')
+    print(locale)
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def hello() -> str:
+    """Return hello world"""
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
