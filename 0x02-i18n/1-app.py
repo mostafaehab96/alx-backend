b668d5e83518e63713a64b707a0bcc905dd21e9e
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-31 22:01:47 +0200

    Add task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Simple flask app."""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config(object):
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def hello():
+    """Return hello world"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
