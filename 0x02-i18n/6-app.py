243b3332e00cb22605347da79876962ccbc9e408
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-11-01 00:33:34 +0200

    Add task 6

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""Simple flask app."""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+class Config(object):
+    """Configuration class"""
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determines best local match"""
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    elif g.user and g.user.locale:
+        return g.user.locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """Returns the user if exists"""
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id), None)
+    return None
+
+
+@app.before_request
+def before_request():
+    """Gets user before any request"""
+    g.user = get_user()
+
+
+@app.route("/")
+def hello() -> str:
+    """Return hello world"""
+    return render_template("5-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
