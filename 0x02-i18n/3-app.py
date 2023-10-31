0ff03837a36338c9aa5da60c74d669c3c25e8a28
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-31 23:46:07 +0200

    Add some configuration

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -5,12 +5,13 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 class Config(object):
     """Configuration class"""
+    DEBUG = True
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -20,13 +21,13 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Determines best local match"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/")
-def hello():
+def hello() -> str:
     """Return hello world"""
     return render_template("3-index.html")
 
